9dd8e24d673c6f80d46da502cfa8400a615a5068
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2024-05-07 09:42:16 +0530

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="w-full bg-cyan-700 text-center fixed bottom-0 ">Made by 🚀 Ambika Kumar Kewat.</div>;
+  return <div className="w-full bg-cyan-600 text-center fixed bottom-0 ">Made by 🚀 Ambika Kumar Kewat.</div>;
 };
 
 export default Footer;
