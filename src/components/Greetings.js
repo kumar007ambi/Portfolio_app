5c1f870458622040ce238a1ede3e3dc9d943f36f
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2024-05-09 20:18:08 +0530

    make greeting page better

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import Animation from "../components/assets/images/Animation - 1715094740022.json";
+import WaveAnimation from "../components/assets/images/WaveAnimation - 1715256921577.json";
+import Lottie from "lottie-react";
+import ContactButton from "./ContactButton";
+import ReseumeButton from "./ReseumeButton";
 
 const Greetings = () => {
+  const style = {
+    width: 80,
+  };
   return (
-    <div>Greetings</div>
-  )
-}
+    <div className="flex ">
+      <div className="w-fit  text-5xl m-[25px] p-[70px]">
+        <p className="text-6xl">
+          Hi all, I'm Ambika
+          <span className="inline-block inset-y-0">
+            <Lottie style={style} animationData={WaveAnimation} />
+          </span>
+        </p>
+        <p className="text-4xl text-blue-500">
+          A passionate Frontend Engineer 🚀 having an experience of building Web
+          applications with JavaScript / Reactjs / TailWindCSS / Bootstrap and
+          some other cool libraries and frameworks.
+        </p>
+        <div className="flex text-2xl space-x-8 ">
+          <ContactButton />
+          <ReseumeButton/>
+        </div>
+      </div>
+      <div>
+      {/* <div className="w-fit m-[16px] p-[20px]"> */}
+        <Lottie animationData={Animation} />
+      </div>
+    </div>
+  );
+};
 
-export default Greetings
+export default Greetings;
---
+++ src/components/ReseumeButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ReseumeButton = () => {
+  return (
+    <>
+      <p className="bg-cyan-600 w-28 cursor-pointer rounded-lg text-black p-2 m-4 text-center hover:bg-blue-300 ">Resume</p>
+    </>
+  );
+};
+
+export default ReseumeButton;
