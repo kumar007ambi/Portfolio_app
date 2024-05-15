e8419e3dda65f2c6b482d271b740807ead21899f
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2024-05-15 22:10:42 +0530

    work experience in progress

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -10,13 +10,12 @@ const workExperiences = {
       desc: " Currently member of Frontend team which deals with a Government Project (Passport Seva) which is maintained by Ministry of External of Affairs.",
       descBullets: [
         "Implemented new features and enhancements for the client’s web application, ensuring functionality aligned with business requirements which leads to increase app’s performance by 30%.",
-        " Optimized existing codebase, resulting in a 20% improvement in application performance.",
       ],
     },
     {
       role: "FrontEnd Developer",
       company: "Webgen Technologies ",
-      companylogo: require("../components/assets/company/WEBGEN.png"),
+      companylogo: require("../components/assets/company/WEBGEN.jpg"),
       date: "May 2021 – Aug 2021",
       desc: "Designed and developed a full‑stack application from scratch, enabling user registration, login, and personalized dashboards. (Keywords: Full‑Stack, application development, user management).",
       descBullets: [
@@ -29,32 +28,17 @@ const workExperiences = {
 const WorkExperience = () => {
   return (
     <>
-      <div className="items-center flex flex-wrap">
+      <div className="flex justify-center ">
         {workExperiences.experience.map((item, i) => {
           return (
-            <div key={i} className="w-[500px] m-3 p-2 ">
-              <div class="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
-                <div class="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
-                  <img
-                    src={item.companylogo}
-                    alt="company-logo"
-                    className="w-full"
-                  />
-                </div>
-                <div class="p-6">
-                  <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-                    {item.role}
-                  </h5>
-                  <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-                    {item.date}
-                  </p>
-                  <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-                    {item.desc}
-                  </p>
-                  <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-                    {item.descBullets}
-                  </p>
-                </div>
+            <div key={i} className="">
+              <div  className="bg-cyan-300 w-[400px] ">
+                <p className="font-bold text-xl">{item.company}</p>
+                <div className="w-"><img src={item.companylogo} alt="company-logo" /></div>
+                <p>{item.role}</p>
+                <p>{item.date}</p>
+                <p>{item.desc}</p>
+                <p>{item.descBullets}</p>
               </div>
             </div>
           );
