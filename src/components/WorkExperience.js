d8410cf74a045f11d66ede31e5be33dbaf872c3b
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2024-05-16 21:59:09 +0530

    work experience in progress

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -6,7 +6,7 @@ const workExperiences = {
       role: "FrontEnd Developer",
       company: "Tata Consultancy Services",
       companylogo: require("../components/assets/company/TCS.png"),
-      date: "June 2018 – Present",
+      date: "Feb 2022 – Present",
       desc: " Currently member of Frontend team which deals with a Government Project (Passport Seva) which is maintained by Ministry of External of Affairs.",
       descBullets: [
         "Implemented new features and enhancements for the client’s web application, ensuring functionality aligned with business requirements which leads to increase app’s performance by 30%.",
@@ -14,7 +14,7 @@ const workExperiences = {
     },
     {
       role: "FrontEnd Developer",
-      company: "Webgen Technologies ",
+      company: "WebGen Technologies ",
       companylogo: require("../components/assets/company/WEBGEN.jpg"),
       date: "May 2021 – Aug 2021",
       desc: "Designed and developed a full‑stack application from scratch, enabling user registration, login, and personalized dashboards. (Keywords: Full‑Stack, application development, user management).",
@@ -28,17 +28,17 @@ const workExperiences = {
 const WorkExperience = () => {
   return (
     <>
-      <div className="flex justify-center ">
+      <div className="flex justify-evenly ">
         {workExperiences.experience.map((item, i) => {
           return (
             <div key={i} className="">
               <div  className="bg-cyan-400 w-[400px] ">
-                <p className="font-bold text-xl">{item.company}</p>
-                <div className="w-"><img src={item.companylogo} alt="company-logo" /></div>
-                <p>{item.role}</p>
-                <p>{item.date}</p>
-                <p>{item.desc}</p>
-                <p>{item.descBullets}</p>
+                <p className="font-bold text-xl text-center">{item.company}</p>
+                <div className="flex justify-center items-center rounded-xl"><img src={item.companylogo} alt="company-logo" /></div>
+                <p className="text-center font-bold text-xl antialiased">{item.role}</p>
+                <p className="text-center text-lg">{item.date}</p>
+                <p className="font-serif text-pretty">- {item.desc}</p>
+                <p className="font-serif text-pretty">- {item.descBullets}</p>
               </div>
             </div>
           );
