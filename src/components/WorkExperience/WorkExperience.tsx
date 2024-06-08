428ab9b4cd488c4a448d906d12b15a17f1370c35
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2024-06-08 21:58:02 +0530

    work on wrokexperince page

--- src/components/WorkExperience/WorkExperience.tsx
+++ src/components/WorkExperience/WorkExperience.tsx
@@ -3,96 +3,119 @@ import ScrollAnimation from "react-animate-on-scroll";
 
 export function WorkExperience() {
   return (
-    <Container id="workexperience">
-      <h2>My Work Experience</h2>
-      <div className="workexperiences">
-        <ScrollAnimation animateIn="flipInY">
-          <div className="workexperience">
-            <header style={{ display: "flex", justifyContent: "center" }}>
-              <a href="https://www.tcs.com/" target="_blank" rel="noreferrer">
-                <h3 style={{color:"white"}}>Tata Consultancy Services</h3>
-              </a>
-            </header>
-            <div className="body">
-              <p style={{ display: "flex", justifyContent: "center" }}>
-                <img
-                  src={require("../assets/company/TCS.png")}
-                  alt="Netflix_GPT"
-                />
-              </p>
-              <p>
-                Currently member of Frontend team which deals with a Government
-                Project (Passport Seva) which is maintained by Ministry of
-                External of Affairs.
-              </p>
-              <p>
-                Implemented new features and enhancements for the client’s web
-                application, ensuring functionality aligned with business
-                requirements which leads to increase app’s performance by 30%.
-              </p>
-            </div>
-            <footer>
-              <p>Feb 2022 – Present</p>
-            </footer>
-          </div>
-        </ScrollAnimation>
+    // <Container id="workexperience">
+    //   <h2>My Work Experience</h2>
+    //   <div className="workexperiences">
+    //     <ScrollAnimation animateIn="flipInY">
+    //       <div className="workexperience">
+    //         <header style={{ display: "flex", justifyContent: "center" }}>
+    //           <a href="https://www.tcs.com/" target="_blank" rel="noreferrer">
+    //             <h3 style={{color:"white"}}>Tata Consultancy Services</h3>
+    //           </a>
+    //         </header>
+    //         <div className="body">
+    //           <p style={{ display: "flex", justifyContent: "center" }}>
+    //             <img
+    //               src={require("../assets/company/TCS.png")}
+    //               alt="Netflix_GPT"
+    //             />
+    //           </p>
+    //           <p>
+    //             Currently member of Frontend team which deals with a Government
+    //             Project (Passport Seva) which is maintained by Ministry of
+    //             External of Affairs.
+    //           </p>
+    //           <p>
+    //             Implemented new features and enhancements for the client’s web
+    //             application, ensuring functionality aligned with business
+    //             requirements which leads to increase app’s performance by 30%.
+    //           </p>
+    //         </div>
+    //         <footer>
+    //           <p>Feb 2022 – Present</p>
+    //         </footer>
+    //       </div>
+    //     </ScrollAnimation>
 
-        <ScrollAnimation animateIn="flipInY">
-          <div className="workexperience">
-            <header style={{ display: "flex", justifyContent: "center" }}>
-              <a
-                href="https://www.webgentechnologies.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <h3 style={{color:"white"}}>WebGen Technologies</h3>
-              </a>
-            </header>
-            <div className="body">
-              <p style={{ display: "flex", justifyContent: "center" }}>
-                {" "}
-                <img
-                  src={require("../assets/company/WEBGEN.jpg")}
-                  alt="Netflix_GPT"
-                />
-              </p>
-              <p>
-                Designed and developed a full‑stack application from scratch,
-                enabling user registration, login, and personalized dashboards.
-                (Keywords: Full‑Stack, application development, user
-                management).
-              </p>
-              <p>
-                Successfully integrated the Stripe API to streamline the payment
-                process within the application. (Keywords: API integration,
-                Stripe, payment processing).
-              </p>
-            </div>
-            <footer>
-              <p>May 2021 – Aug 2021</p>
-            </footer>
+    //     <ScrollAnimation animateIn="flipInY">
+    //       <div className="workexperience">
+    //         <header style={{ display: "flex", justifyContent: "center" }}>
+    //           <a
+    //             href="https://www.webgentechnologies.com/"
+    //             target="_blank"
+    //             rel="noreferrer"
+    //           >
+    //             <h3 style={{color:"white"}}>WebGen Technologies</h3>
+    //           </a>
+    //         </header>
+    //         <div className="body">
+    //           <p style={{ display: "flex", justifyContent: "center" }}>
+    //             {" "}
+    //             <img
+    //               src={require("../assets/company/WEBGEN.jpg")}
+    //               alt="Netflix_GPT"
+    //             />
+    //           </p>
+    //           <p>
+    //             Designed and developed a full‑stack application from scratch,
+    //             enabling user registration, login, and personalized dashboards.
+    //             (Keywords: Full‑Stack, application development, user
+    //             management).
+    //           </p>
+    //           <p>
+    //             Successfully integrated the Stripe API to streamline the payment
+    //             process within the application. (Keywords: API integration,
+    //             Stripe, payment processing).
+    //           </p>
+    //         </div>
+    //         <footer>
+    //           <p>May 2021 – Aug 2021</p>
+    //         </footer>
+    //       </div>
+    //     </ScrollAnimation>
+    //   </div>
+    // </Container>
+    <Container id="workexperience">
+      <div className="workexperience-left">
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000}>
+         
+          <div style={{ height: "100%", width: "100%" }}>
+            <img src={require("../assets/company/TCS.png")} alt="TCS" />
+          </div>
+          <div>
+           <p>
+              Currently member of Frontend team which deals with a Government
+              Project (Passport Seva) which is maintained by Ministry of
+              External of Affairs.
+            </p>
+            <p>
+              Implemented new features and enhancements for the client’s web
+             application, ensuring functionality aligned with business
+             requirements which leads to increase app’s performance by 30%.
+             </p>
           </div>
         </ScrollAnimation>
+      </div>
+      <div className="workexperience-right">
+        <ScrollAnimation animateIn="fadeInRight" delay={0.2 * 1000}>
+        <div style={{ height: "100%", width: "100%" }}>
+            <img src={require("../assets/company/TCS.png")} alt="TCS" />
+          </div>
+          <div>
+           <p>
+                 Designed and developed a full‑stack application from scratch,
+                 enabling user registration, login, and personalized dashboards.
+                 (Keywords: Full‑Stack, application development, user
+                 management).
+               </p>
+               <p>
+                 Successfully integrated the Stripe API to streamline the payment
+                 process within the application. (Keywords: API integration,
+                 Stripe, payment processing).
+               </p>
+          </div>
+        </ScrollAnimation>   
       </div>
     </Container>
   );
 }
-
-//  <>
-//       <div id="workexperience" className="flex justify-evenly h-[500px]">
-//         {workExperiences.experience.map((item, i) => {
-//           return (
-//             <div key={i} className="p-6 m-2 shadow-slate-950">
-//               <div  className="bg-green-200 rounded-xl shadow-lg shadow-indigo-400/40 hover:shadow-indigo-800/80 w-[400px] ">
-//                 <p className="font-bold text-xl text-center">{item.company}</p>
-//                 <div className="flex justify-center items-center "><img className="rounded-xl" src={item.companylogo} alt="company-logo" /></div>
-//                 <p className="text-center font-bold text-xl antialiased">{item.role}</p>
-//                 <p className="text-center text-lg">{item.date}</p>
-//                 <p className="font-serif text-pretty">- {item.desc}</p>
-//                 <p className="font-serif text-pretty">- {item.descBullets}</p>
-//               </div>
-//             </div>
-//           );
-//         })}
-//       </div>
-//     </>
--- src/components/WorkExperience/styles.ts
+++ src/components/WorkExperience/styles.ts
@@ -2,104 +2,193 @@ import styled from "styled-components";
 
 
 export const Container = styled.section`
-  margin-top: 15rem;
+  // margin-top: 15rem;
   
-  h2{
-    text-align: center;
-    font-size: 4rem;
-    margin-bottom: 8px;
-  }
-  .workexperiences{
-    display: grid;
-    grid-template-columns: repeat(2, 2fr);
-    grid-template-rows: auto;
-    gap: 3rem;
-    padding: 2rem;
-    overflow: hidden;
-    width:1000px;
-    margin: auto;
+  // h2{
+  //   text-align: center;
+  //   font-size: 4rem;
+  //   margin-bottom: 8px;
+  // }
+  // .workexperiences{
+  //   display: grid;
+  //   grid-template-columns: repeat(3, 1fr);
+  //   grid-template-rows: auto;
+  //   gap: 2rem;
+  //   padding: 1rem;
+  //   overflow: hidden;
     
-    .workexperience{
-      padding: 2rem 1.8rem;
-      background-color: #2b2b2b;
-      border-radius: 1.2rem;
-      transition: 0.25s;
-      display: flex;
-      flex-direction: column;
-      height: 100%;
-      color: #FFF;
+  //   // margin: auto;
+    
+  //   .workexperience{
+  //     padding: 2rem 1.8rem;
+  //     background-color: #2b2b2b;
+  //     border-radius: 1.2rem;
+  //     transition: 0.25s;
+  //     display: flex;
+  //     flex-direction: column;
+  //     height: 100%;
+  //     color: #FFF;
       
-      &:hover{
-        transform: translateY(-5px);
-        background-color: var(--pink);
-      }
+  //     &:hover{
+  //       transform: translateY(-5px);
+  //       background-color: var(--pink);
+  //     }
 
-      header{
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        color: var(--blue);
-        margin-bottom: 6px;
-        // .workexperience-links{
-        //   display: flex;
-        //   align-items: center;
-        //   gap: 1rem;
-        // }
-        a > img {
-          width: 5.0rem;
-        }
-      }
+  //     header{
+  //       display: flex;
+  //       align-items: center;
+  //       justify-content: space-between;
+  //       color: var(--blue);
+  //       margin-bottom: 6px;
+  //       // .workexperience-links{
+  //       //   display: flex;
+  //       //   align-items: center;
+  //       //   gap: 1rem;
+  //       // }
+  //       a > img {
+  //         width: 5.0rem;
+  //       }
+  //     }
       
-      h3{
-        margin-bottom: 2rem;
-        text-align:center;
-      }
+  //     h3{
+  //       margin-bottom: 2rem;
+  //       text-align:center;
+  //     }
 
-      p{
-        letter-spacing: 0.12rem;
-        margin-bottom: 2rem;
-        text-align:center;
-        a{
-          color: #FFFF;
-          border-bottom: 1px solid var(--green);
-          transition: color 0.25s;
-          &:hover{
-            color: var(--green);
-          }
-        }
-      }
-      p > img{
-         height: 125px;
-         width: 270px;
-         border-radius: 4px;
-      }
+  //     p{
+  //       letter-spacing: 0.12rem;
+  //       margin-bottom: 2rem;
+  //       text-align:center;
+  //       a{
+  //         color: #FFFF;
+  //         border-bottom: 1px solid var(--green);
+  //         transition: color 0.25s;
+  //         &:hover{
+  //           color: var(--green);
+  //         }
+  //       }
+  //     }
+  //     p > img{
+  //        height: 125px;
+  //        width: 270px;
+  //        border-radius: 4px;
+  //     }
 
-      footer{
-        margin-top: auto;
+  //     footer{
+  //       margin-top: auto;
        
-        .tech-list{
-          display: flex;
-          align-items: center;
-          gap: 2rem;
-          font-size: 1.4rem;
-          opacity: 0.6;
-        }
-      }
+  //       .tech-list{
+  //         display: flex;
+  //         align-items: center;
+  //         gap: 2rem;
+  //         font-size: 1.4rem;
+  //         opacity: 0.6;
+  //       }
+  //     }
 
+  //   }
+  // }
+
+  // @media (max-width: 960px){
+  //   .workexperiences{
+  //     grid-template-columns:2fr 2fr;
+  //     width:610px;
+  //   }
+  // }
+
+  // @media (max-width: 740px){
+  //   .workexperiences{
+  //     grid-template-columns: 1fr;
+  //     // width:410px;
+  //   }
+  // }
+  margin-top: 12rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2rem;
+
+  .hard-skills{
+    margin-top: 1.6rem;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 1.8rem;
+  }
+  .hability{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    img{
+      width: 3.4rem;
+    }
+  }
+
+  h2{
+    display: inline-block;
+    margin-bottom: 2rem;
+    font-size :3rem;
+    margin-top: 0rem;
+    color: var(--green);
+  }
+
+  h3{
+    margin-top: 2rem;
+    color: var(--green);
+  }
+
+  p{
+    font-size: 1.8rem;
+    letter-spacing: 0.1rem;
+    font-weight: 500;
+  }
+  
+  .skills-image{
+    text-align: center;
+    img{
+     margin-top: 2rem;
+     width: 75%;
+     filter: grayscale(0);
+     transition: filter 0.5s;
+     &:hover{
+       filter: grayscale(0);
+     }
+   }
+  }
+
+  @media only screen and (max-width: 480px) {
+    .skills-image {
+      max-width: 100%;
+      margin-top: 4rem;
+      // img{
+      //   margin-top: 2rem;
+      //   width: 100%;
+      //   filter: grayscale(0);
+      //   transition: filter 0.5s;
+      //   &:hover{
+      //     filter: grayscale(0);
+      //   }
     }
   }
 
   @media (max-width: 960px){
-    .workexperiences{
-      grid-template-columns: 2fr;
-      width:610px;
+    display: block;
+    text-align: center;
+    
+    .hard-skills{
+      justify-content: center;
     }
-  }
-
-  @media (max-width: 740px){
-    .workexperiences{
-      grid-template-columns: 1fr;
-      width:410px;
+    .skills-image{
+      display: flex;
+      max-width: 100%;
+      // img{
+      //   margin-top: 2rem;
+      //   width: 100%;
+      //   filter: grayscale(0);
+      //   transition: filter 0.5s;
+      //   &:hover{
+      //     filter: grayscale(0);
+      //   }
     }
-  }
+}
 `
